5e738834a5c7fef66fcf320733daafdc20a24f87
Robbie Behm <behmaster@gmail.com>
2022-07-25 23:02:14 -0500

    fixed errors

--- src/Store.js
+++ src/Store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './features/counterSlice'
-import todoReducer from './features/toDoSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counterSlice";
+import todoReducer from "./features/toDoSlice";
 
 export const store = configureStore({
-    reducer: counterReducer,
-    todos: todoReducer
-})
+  reducer: {
+    counter: counterReducer,
+    todos: todoReducer,
+  },
+});
